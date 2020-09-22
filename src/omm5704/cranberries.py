ea4903a0a7ae20e00fd5f5278038e65f426413aa
kittell <kirk.kittell@gmail.com>
2020-09-22 15:23:29 -0500

    various updates, working on case

--- src/omm5704/cranberries.py
+++ src/omm5704/cranberries.py
@@ -19,25 +19,45 @@ class BulkTruck:
         self.load_current = self.load_initial
         self.load_dry = self.useful_ratio * self.load_initial
         
+    def adjust_load(self, load_diff):
+        self.load_current += load_diff
+        # TODO: how to handle load < 0
+        
        
 class Bin:
     def __init__(self):
-        self.volume_filled = 0
+        self.filled = 0
+        self.capacity = 0
+        
+    def adjust_contents(self, contents_diff):
+        remaining = 0
+        if contents_diff > 0 :
+            adjust_bin = min(self.capacity - self.filled, contents_diff)
+        else:
+            adjust_bin = min(self.filled, -contents_diff)
+            
+        self.filled += adjust_bin
+        remaining -= adjust_bin
+        print(self.berry_type, adjust_bin, self.filled)
+        return remaining
 
 class WetBin(Bin):
     def __init__(self):
+        super().__init__()
         self.berry_type = 'wet'
-        self.volume = 400
+        self.capacity = 400
         
 class WetDryBin(Bin):
     def __init__(self, berry_type):
+        super().__init__()
         self.berry_type = berry_type
-        self.volume = 250
+        self.capacity = 250
         
 class DryBin(Bin):
     def __init__(self):
+        super().__init__()
         self.berry_type = 'dry'
-        self.volume = 250
+        self.capacity = 250
 
 class Dumper:
     def __init__(self, model_settings):
@@ -56,8 +76,7 @@ class Dumper:
     
     def model_step(self, time_step):
         if self.truck_id is not None:
-            if self.truck_id > 0:
-                self.time_remaining -= time_step
+            self.time_remaining -= time_step
         
 class ReceivingFacility:
     def __init__(self, model_settings):
@@ -73,10 +92,12 @@ class ReceivingFacility:
         self.bins_wet = list()
         self.capacity_wet = 0
         self.filled_wet = 0
+        self.empty_wet = 0
         
         self.bins_dry = list()
         self.capacity_dry = 0
         self.filled_dry = 0
+        self.empty_dry = 0
         
         self.initialize_bins(model_settings)
     
@@ -84,26 +105,38 @@ class ReceivingFacility:
         # Wet bins
         for i in range(model_settings['bins_wet']):
             b = WetBin()
-            self.capacity_wet += b.volume
+            self.capacity_wet += b.capacity
             self.bins_wet.append(b)
             
         # Dry bins
         for i in range(model_settings['bins_dry']):
             b = DryBin()
-            self.capacity_dry += b.volume
-            self.bins_wet.append(b)
+            self.capacity_dry += b.capacity
+            self.bins_dry.append(b)
             
         # Wet/dry bins
         for i in range(model_settings['bins_wetdry_dry']):
             b = WetDryBin('dry')
-            self.capacity_dry += b.volume
+            self.capacity_dry += b.capacity
             self.bins_dry.append(b)
             
         for i in range(model_settings['bins_wetdry_wet']):
             b = WetDryBin('wet')
-            self.capacity_wet += b.volume
+            self.capacity_wet += b.capacity
             self.bins_wet.append(b)
-                     
+        
+        self.update_bin_status()
+    
+    def update_bin_status(self):        
+        self.filled_dry = 0
+        for b in self.bins_dry:
+            self.filled_dry += b.filled
+        self.empty_dry = self.capacity_dry - self.filled_dry
+            
+        self.filled_wet = 0
+        for b in self.bins_wet:
+            self.filled_wet += b.filled
+        self.empty_wet = self.capacity_wet - self.filled_wet
         
     def get_test_time(self):
         return 0
@@ -114,9 +147,9 @@ class ReceivingFacility:
         for d in self.dumpers:
             self.dumpers[d].model_step(time_step)
         
+        self.assign_trucks_to_dumpers()
         self.transfer_load_to_bin()
         self.dismiss_trucks()
-        self.assign_trucks_to_dumpers()
         
     
     def dismiss_trucks(self):
@@ -124,8 +157,11 @@ class ReceivingFacility:
             truck_id = self.dumpers[d].truck_id
             if truck_id is not None:
                 if self.dumpers[d].time_remaining <= 0:
-                    self.dumpers[d].initialize_dumper()
-                    self.truck_queue.truck_leaves(truck_id)
+                    truck = self.truck_queue.get_truck(truck_id)
+                    if truck.load_current <= 0:
+                        # Only dismiss the truck if it has transferred its load
+                        self.dumpers[d].initialize_dumper()
+                        self.truck_queue.truck_leaves(truck_id)
     
     def assign_trucks_to_dumpers(self):
         # Find first open dumper
@@ -138,10 +174,48 @@ class ReceivingFacility:
                         # Assign a truck to the empty dumper
                         truck.at_dumper = True
                         self.dumpers[d].assign_truck(truck.truck_id)
+                        break
                         
         
     def transfer_load_to_bin(self):
-        pass
+        # TODO: useful abstraction: dumper ready for truck to dump, (maybe short delay), leave/pop
+        
+        # If all the bins are full, skip the rest
+        if self.empty_dry == 0 and self.empty_wet == 0:
+            return
+        
+        for d in self.dumpers:
+            if self.dumpers[d].truck_id is not None:
+                if self.dumpers[d].time_remaining <= 0:
+                    # Ready to transfer
+                    truck = self.truck_queue.get_truck(self.dumpers[d].truck_id)
+                    
+                    # Find an open bin, if there is one
+                    # TODO: feels like an abstraction hiding here
+                    berry_type = truck.berry_type
+                    if berry_type == 'dry':
+                        if self.empty_dry > 0:
+                            load_transfer = min(truck.load_current, self.empty_dry)
+                            remaining_transfer = load_transfer
+                            
+                            for b in self.bins_dry:
+                                # Add load to bin
+                                remaining_transfer = b.adjust_contents(remaining_transfer)
+                                
+                    else:
+                        if self.empty_wet > 0:
+                            load_transfer = min(truck.load_current, self.empty_wet)
+                            remaining_transfer = load_transfer
+                            
+                            for b in self.bins_wet:
+                                # Add load to bin
+                                remaining_transfer = b.adjust_contents(remaining_transfer)
+                    
+                    # Remove load from truck
+                    truck.adjust_load(-load_transfer)
+                    # Update bin status
+                    self.update_bin_status()
+                    
     
     def print_output(self):
         output = ''
@@ -165,6 +239,13 @@ class TruckQueue(list):
 
         self.truck_arrives()
         self.truck_waits(time_step)
+    
+    def get_truck(self, get_id):
+        for truck in self:
+            if truck.truck_id == get_id:
+                return truck
+            
+#         return None
     
     def truck_waits(self, time_step):
         # Count the total amount of wait time of trucks over the course of the model
